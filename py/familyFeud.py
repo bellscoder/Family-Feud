65ac84bf7386e665d17b29cec94f27e27bae2dc8
Ruger-Allen <ruger.allen@bellsisd.net>
2025-10-31 10:00:15 -0500

    Update familyFeud.py

--- py/familyFeud.py
+++ py/familyFeud.py
@@ -11,13 +11,7 @@ from question_1 import question_1_dict
 from question_2 import question_2_dict
 from question_3 import question_3_dict
 
-choosing = random.randint(1,3)
-if choosing == 1:
-  current_dict = question_1_dict
-elif choosing == 2:
-  current_dict = question_2_dict
-else:
-  current_dict = question_3_dict
+current_dict = question_1_dict
 # Show the intro to the game
 function_defs.intro()
 
@@ -30,8 +24,6 @@ team_1_points = 0
 team_2_points = 0
 running = True
 
-times = 0
-
 correct_answer = False
 
 ## Strikes
@@ -43,42 +35,47 @@ elif not team_1:
   current_guess = guesses_2
 ## While Loop
 while running:
-    if team_1:
-      print("It is currently team one's turn, with " + str(guesses_1) + " guesses remaining.")
-    else:
-      print("It is currently team two's turn, with " + str(guesses_2) + " guesses remaining.")
-    
-    # Print the first question
-    q1 = current_dict
-    print(q1["question"])
-    answer_to_q1 = input()
-    
-    correct1 = function_defs.check_answer1(answer_to_q1,guesses_1,question_1_dict)
-    print(correct1)
-    if not correct1:
-      current_guess -= 1
-    
-    if current_guess == 0 and times == 0:
-      team_1 = False
-      times += 1
-    elif current_guess == 0 and times == 1:
-      team_1 = True
-      times = 0
-    answer = answer_to_q1.lower()
-  
-    if answer in question_1_dict["answers"]:
-      points = (question_1_dict["answers"][answer])
-      correct_answer = True
-      
-    if correct_answer and times == 0:
-      team_1_points += points
-      correct_answer = False
-      
-    elif correct_answer and times == 1:
-      team_2_points += points
-      correct_answer = False
 
-  print(f"Team 1's score is [team_1_points].")
-  print(f"Team 2's score is [team_2_points].")
+  if times == 3:
+    current_dict = question_2_dict
+  if times == 5:
+    current_dict = question_3_dict
+  if team_1:
+    print("It is currently team one's turn, with " + str(guesses_1) + " guesses remaining.")
+  else:
+    print("It is currently team two's turn, with " + str(guesses_2) + " guesses remaining.")
+  
+  # Print the first question
+  q1 = current_dict
+  print(q1["question"])
+  answer_to_q1 = input()
+  
+  correct1 = function_defs.check_answer1(answer_to_q1,guesses_1,question_1_dict)
+  print(correct1)
+  if not correct1:
+    current_guess -= 1
+  
+  if current_guess == 0 and times == 0 or times == 2 or times == 4 or times == 6:
+    team_1 = False
+    times += 1
+  elif current_guess == 0 and times == 1 or times == 3 or times == 5 or times == 7:
+    team_1 = True
+    times += 1
+  answer = answer_to_q1.lower()
+
+  if answer in question_1_dict["answers"]:
+    points = (question_1_dict["answers"][answer])
+    correct_answer = True
+    
+  if correct_answer and times == 0:
+    team_1_points += points
+    correct_answer = False
+    
+  elif correct_answer and times == 1 or times == 3:
+    team_2_points += points
+    correct_answer = False
+
+print(f"Team 1's score is [team_1_points].")
+print(f"Team 2's score is [team_2_points].")
 if not running:
-  function_defs.outro(question_1_dict,question_2_dict,question_3_dict)
+function_defs.outro(question_1_dict,question_2_dict,question_3_dict)
